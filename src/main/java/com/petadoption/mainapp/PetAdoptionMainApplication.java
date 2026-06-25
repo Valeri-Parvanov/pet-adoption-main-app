5f1f657baec82b58db321ee3271a68595c8be797
Valeri Parvanov <valyo.parvanov@gmail.com>
2026-06-25 14:05:48 +0300

    chore: init Spring Boot project structure
    Spring Boot 4.1.0, Java 21, Maven. Added Spring Web, Data JPA, Security, Thymeleaf, MySQL driver, Validation dependencies.

---
+++ src/main/java/com/petadoption/mainapp/PetAdoptionMainApplication.java
@@ -0,0 +1,13 @@
+package com.petadoption.mainapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetAdoptionMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetAdoptionMainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/petadoption/mainapp/PetAdoptionMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petadoption.mainapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetAdoptionMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
