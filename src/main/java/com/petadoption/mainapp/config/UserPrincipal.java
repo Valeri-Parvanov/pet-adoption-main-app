603753f10bd9a56c501a725c289514258ab192b4
Valeri Parvanov <valyo.parvanov@gmail.com>
2026-06-28 17:36:56 +0300

    feat: add Spring Security config and public home page

---
+++ src/main/java/com/petadoption/mainapp/config/UserPrincipal.java
@@ -0,0 +1,35 @@
+package com.petadoption.mainapp.config;
+
+import com.petadoption.mainapp.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class UserPrincipal implements UserDetails {
+
+    private final User user;
+
+    public UserPrincipal(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Set<GrantedAuthority> getAuthorities() {
+        return user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.getName().name()))
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+}
